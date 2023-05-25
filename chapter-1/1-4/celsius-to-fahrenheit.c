62e4b4d734bf9939a9464a86c8b16c034c8248d2
Sei-Jin <kajiquiet@gmail.com>
2023-05-25 05:59:16 -0300

    Add 1-4

---
+++ chapter-1/1-4/celsius-to-fahrenheit.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+// print Celsius-Fahrenheit table
+// for celsius = 0, 20, ..., 300; floating-point version
+
+int main()
+{
+	float fahr, celsius;
+	float lower, upper, step;
+
+	lower = 0; 
+	upper = 300;
+	step = 20;
+
+	fahr = lower;
+	
+	// print heading
+	printf("Celsius Fahrenheit\n");
+	printf("------------------\n");
+
+	// print values
+	while (celsius <= upper)
+	{
+		fahr = (9.0/5.0) * (celsius+32.0);
+		printf("%7.0f %10.1f\n", celsius, fahr);
+		celsius = celsius + step;
+	}
+}
+
+
